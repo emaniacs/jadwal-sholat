54111214dc93b7051b02f800edb943e87c080221
Novi Ardi <noone.nu1@gmail.com>
2024-01-09 20:52:57 +0700

    use command builder

--- src/main.rs
+++ src/main.rs
@@ -6,51 +6,84 @@ mod jadwal;
 use std::env;
 
 use chrono::{Local, NaiveDate};
-use clap::Parser;
+use clap::{Arg, ArgAction, ArgMatches, Command};
 
-#[derive(Parser, Debug, Default)]
-#[command(author, version, about)]
-struct Args {
-    date: Option<String>,
+fn create_command() -> Command {
+    Command::new("jadwal-shalat")
+        .arg(Arg::new("date").help("Date of jadwal shalat, default today, format YEAR-MONTH-DAY"))
+        .arg(
+            Arg::new("provinsi")
+                .short('p')
+                .long("provinsi")
+                .required(false)
+                .help("Provinsi, if not provided will read env JADWAL_PROVINSI "),
+        )
+        .arg(
+            Arg::new("kabupaten")
+                .short('k')
+                .long("kabupaten")
+                .required(false)
+                .help("Kabupaten, if not provided will read env JADWAL_KABUPATEN "),
+        )
+        .arg(
+            Arg::new("list-daerah")
+                .long("list-daerah")
+                .short('l')
+                .action(ArgAction::SetTrue)
+                .help("Show list daerah and exit"),
+        )
+        .arg(
+            Arg::new("all-day")
+                .short('a')
+                .action(ArgAction::SetTrue)
+                .long("all-day")
+                .help("Show all jadwal in current date"),
+        )
+}
 
-    #[arg(short, long)]
-    provinsi: Option<String>,
+fn get_provinsi(matches: &ArgMatches) -> Result<String, env::VarError> {
+    match matches.get_one::<String>("provinsi") {
+        Some(val) => Ok(val.to_string()),
+        None => env::var("JADWAL_PROVINSI"),
+    }
+}
 
-    #[arg(short, long)]
-    kabupaten: Option<String>,
-
-    #[arg(long)]
-    list_daerah: bool,
-
-    #[arg(long)]
-    all_day: bool,
+fn get_kabupaten(matches: &ArgMatches) -> Result<String, env::VarError> {
+    match matches.get_one::<String>("kabupaten") {
+        Some(val) => Ok(val.to_string()),
+        None => env::var("JADWAL_KABUPATEN"),
+    }
 }
 
 #[tokio::main]
 async fn main() -> Result<(), reqwest::Error> {
-    let args = Args::parse();
+    let app = create_command();
+    let matches = app.get_matches();
 
-    let date = match args.date {
+    let date = match matches.get_one::<String>("date") {
         Some(val) => NaiveDate::parse_from_str(&val, "%Y-%m-%d").expect("failed parse date"),
         None => Local::now().date_naive(),
     };
 
-    let provinsi: String = match args.provinsi {
-        Some(val) => val.to_uppercase(),
-        None => env::var("JADWAL_PROVINSI")
-            .expect("provinsi not provided in option or env, please set JADWAL_PROVINSI")
-            .to_uppercase(),
+    let provinsi: String = match get_provinsi(&matches) {
+        Ok(val) => val.to_uppercase(),
+        Err(_) => {
+            eprintln!("provinsi not provided either in argument or as env JADWAL_PROVINSI");
+            std::process::exit(255);
+        }
     };
 
-    let kabupaten: String = match args.kabupaten {
-        Some(val) => val.to_uppercase(),
-        None => env::var("JADWAL_KABUPATEN")
-            .expect("provinsi not provided in option or env, please set JADWAL_KABUPATEN")
-            .to_uppercase(),
+    let kabupaten: String = match get_kabupaten(&matches) {
+        Ok(val) => val.to_uppercase(),
+        Err(_) => {
+            eprintln!("kabupaten not provided either in argument or as env JADWAL_KABUPATEN");
+            std::process::exit(255);
+        }
     };
 
     let vec_daerah = daerah::load_daerah().await;
-    if args.list_daerah {
+    let list_daerah = matches.get_one::<bool>("list-daerah").unwrap_or(&false);
+    if *list_daerah {
         daerah::list_daerah(vec_daerah);
         return Ok(());
     }
@@ -67,7 +100,8 @@ async fn main() -> Result<(), reqwest::Error> {
     };
 
     let jadwal = jadwal::load_jadwal(daerah, date).await;
-    if args.all_day {
+    let all_day = matches.get_one::<bool>("all-day").unwrap_or(&false);
+    if *all_day {
         println!(
             "Jadwal Sholat {} {} at {}",
             daerah.kabupaten, daerah.provinsi, date
